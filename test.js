e87ae8e90d6bb79d0ca8385e86885c0e8a48f463
sparklelclc <sparklesese2@163.com>
2015-10-04 06:26:29 +0800

    newthis

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
-var fs = require('fs')
+var fs = require('fs');
+//
 /*
 var mongodb = require('mongodb');
 var mongoserver = new mongodb.Server("0.0.0.0",27017,{});
